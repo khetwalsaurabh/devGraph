8d5e57fc85d7506ea78cc0d717bcf73de9e8e3bb
khetwalsaurabh <ssourabh.1712@gmail.com>
2026-08-24 16:00:57 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,13 @@ const testConnection = async () => {
 
 testConnection();
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
+// app.listen(port,()=>{
+//     console.log(`Server is running on port ${port}`);
     
-})
+// })
 
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server is running on port ${port}`);
+});
 
 
